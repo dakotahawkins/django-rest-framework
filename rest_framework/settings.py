3fa822afaada6686b6a73e6d4b0f563b25e844dc
Dakota Hawkins <dakotahawkins@users.noreply.github.com>
2018-11-07 22:06:58 -0500

    Allow further customization in ApiSettings
    This will help other projects (e.g. [django-rest-framework-jwt](https://github.com/GetBlimp/django-rest-framework-jwt)) that use ApiSettings for their own settings do things like reload in response to settings changes.

--- rest_framework/settings.py
+++ rest_framework/settings.py
@@ -23,7 +23,7 @@ from __future__ import unicode_literals
 from importlib import import_module
 
 from django.conf import settings
-from django.test.signals import setting_changed
+from django.core.signals import setting_changed
 from django.utils import six
 
 from rest_framework import ISO_8601
@@ -198,17 +198,32 @@ class APISettings(object):
     Any setting with string import paths will be automatically resolved
     and return the class, rather than the string literal.
     """
-    def __init__(self, user_settings=None, defaults=None, import_strings=None):
+    def __init__(
+        self,
+        user_settings=None,
+        defaults=None,
+        import_strings=None,
+        removed_settings=None
+        settings_key=None,
+        settings_doc=None
+    ):
+        self._settings_doc = settings_doc or 'https://www.django-rest-framework.org/api-guide/settings/'
+        self._removed_settings = removed_settings or REMOVED_SETTINGS
         if user_settings:
             self._user_settings = self.__check_user_settings(user_settings)
         self.defaults = defaults or DEFAULTS
         self.import_strings = import_strings or IMPORT_STRINGS
-        self._cached_attrs = set()
+        self._settings_key = settings_key or 'REST_FRAMEWORK'
+        self._cached_attrs = set()        
+        setting_changed.connect(self.reload_api_settings)
+        
+    def __del__(self):
+        setting_changed.disconnect(self.reload_api_settings)        
 
     @property
     def user_settings(self):
         if not hasattr(self, '_user_settings'):
-            self._user_settings = getattr(settings, 'REST_FRAMEWORK', {})
+            self._user_settings = getattr(settings, self._settings_key, {})
         return self._user_settings
 
     def __getattr__(self, attr):
@@ -232,10 +247,9 @@ class APISettings(object):
         return val
 
     def __check_user_settings(self, user_settings):
-        SETTINGS_DOC = "https://www.django-rest-framework.org/api-guide/settings/"
-        for setting in REMOVED_SETTINGS:
+        for setting in self._removed_settings:
             if setting in user_settings:
-                raise RuntimeError("The '%s' setting has been removed. Please refer to '%s' for available settings." % (setting, SETTINGS_DOC))
+                raise RuntimeError("The '%s' setting has been removed. Please refer to '%s' for available settings." % (setting, self._settings_doc))
         return user_settings
 
     def reload(self):
@@ -243,16 +257,16 @@ class APISettings(object):
             delattr(self, attr)
         self._cached_attrs.clear()
         if hasattr(self, '_user_settings'):
-            delattr(self, '_user_settings')
+            delattr(self, '_user_settings')            
+    
+    def reload_api_settings(self, *args, **kwargs):
+        setting = kwargs['setting']
+        if setting == self._settings_key:
+            self.reload()
 
 
 api_settings = APISettings(None, DEFAULTS, IMPORT_STRINGS)
 
 
-def reload_api_settings(*args, **kwargs):
-    setting = kwargs['setting']
-    if setting == 'REST_FRAMEWORK':
-        api_settings.reload()
 
 
-setting_changed.connect(reload_api_settings)
